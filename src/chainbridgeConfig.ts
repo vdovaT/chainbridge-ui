7cb56f599e9717cb94da20cebe16f58e83471ef5
Michael Yankelev <myankelev@gmail.com>
2020-11-23 03:23:09 +0200

    update config for protolab

--- src/chainbridgeConfig.ts
+++ src/chainbridgeConfig.ts
@@ -91,6 +91,68 @@ export const chainbridgeConfig: ChainbridgeConfig = {
       ],
     },
   ],
+  // AVAX-C - Goerli - @Arsenii Petrovich - Protofire
+  // chains: [
+  //   {
+  //     chainId: 1,
+  //     networkId: 5,
+  //     name: "Ethereum - Goerli",
+  //     bridgeAddress: "0x0993CEE13a42301d2e050693806fe79E822d42fB",
+  //     erc20HandlerAddress: "0x68b6A17C5E4B08759ee7F2e092356c479C5Cc65d",
+  //     rpcUrl:
+  //       "https://eth-goerli.alchemyapi.io/v2/Yf3zLkgLb5nc_KHg35Szq6k9EzpedEmn",
+  //     type: "Ethereum",
+  //     blockExplorer: "https://goerli.etherscan.io/tx",
+  //     nativeTokenSymbol: "ETH",
+  //     tokens: [
+  //       {
+  //         address: "0x6D16859DE8C9421E52c4F5A6003DAcA3B4f8e36B",
+  //         name: "ERC20",
+  //         symbol: "ERC20",
+  //         imageUri: ETHIcon,
+  //         resourceId:
+  //           "0x0000000000000000000000F1c1e7dfF8466f8A3887AB4a9d398727A700DF9305",
+  //       },
+  //       {
+  //         address: "0x71181Ed7D6f42b594443a4Fd7F0Ce24B08787b77",
+  //         name: "ERC721",
+  //         symbol: "ERC721",
+  //         imageUri: ETHIcon,
+  //         resourceId:
+  //           "0x0000000000000000000000736eF91A34E233630A23b4d02D29b3ea8726AF9C05",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     chainId: 2,
+  //     networkId: 43113,
+  //     name: "AVAX-C Chain",
+  //     bridgeAddress: "0xed52eCA444088D3892EdaEbb05Ccd012165545D3",
+  //     erc20HandlerAddress: "0xd4B2c7B6CB241848Ac38EA94F131263A820c462D",
+  //     rpcUrl: "https://api.avax-test.network/ext/bc/C/rpc",
+  //     type: "Ethereum",
+  //     blockExplorer: "https://cchain.explorer.avax-test.network/tx",
+  //     nativeTokenSymbol: "AVAX",
+  //     tokens: [
+  //       {
+  //         address: "0x6D16859DE8C9421E52c4F5A6003DAcA3B4f8e36B",
+  //         name: "ERC20",
+  //         symbol: "ERC20",
+  //         imageUri: ETHIcon,
+  //         resourceId:
+  //           "0x0000000000000000000000F1c1e7dfF8466f8A3887AB4a9d398727A700DF9305",
+  //       },
+  //       {
+  //         address: "0x71181Ed7D6f42b594443a4Fd7F0Ce24B08787b77",
+  //         name: "ERC721",
+  //         symbol: "ERC721",
+  //         imageUri: ETHIcon,
+  //         resourceId:
+  //           "0x0000000000000000000000736eF91A34E233630A23b4d02D29b3ea8726AF9C05",
+  //       },
+  //     ],
+  //   },
+  // ],
 
   // DEVNET
   //   erc20ResourceId:

ab7dc2d1c7aa1d198abfc5d7a38ca52c27b276db
vdovaT <vdova@protonmail.com>
2022-03-18 18:48:25 +0800

    push

--- src/Layouts/AppHeader.tsx
+++ src/Layouts/AppHeader.tsx
@@ -68,7 +68,7 @@ const AppHeader: React.FC<IAppHeader> = () => {
         <div className={classes.logo}>
           <img alt="Moonbeam logo" src={logo}></img>
         </div>
-        <Typography variant="h4">Teddy Alpha ChainBridge</Typography>
+        <Typography variant="h4">Teddy Doge ChainBridge</Typography>
       </div>
       <section className={classes.state}>
         {!isReady ? (

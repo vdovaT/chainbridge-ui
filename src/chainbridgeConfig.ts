25dfcd03def672dcffa63988c2d3b0bbfcff85f7
vdovaT <vdova@protonmail.com>
2022-04-24 00:48:02 +0800

    update

--- src/chainbridgeConfig.ts
+++ src/chainbridgeConfig.ts
@@ -203,6 +203,59 @@ export const chainbridgeConfig: ChainbridgeConfig = {
       ],
     },
     {
+      chainId: 37,
+      networkId: 137,
+      name: "Ethereum - MATIC",
+      bridgeAddress: "0x9cf48a0b0c6127Aa5d934e92E16099C4233F4D20",
+      erc20HandlerAddress: "0x998598E9eD523Fd52c0B93a3272336Ae13D5c3C5",
+      rpcUrl: `https://speedy-nodes-nyc.moralis.io/4a0481cf5a19c72502361ffc/polygon/mainnet`,
+      type: "Ethereum",
+      blockExplorer: "https://polygonscan.com/",
+      nativeTokenSymbol: "MATIC",
+      tokens: [
+        {
+          address: "0x10f6f2b97f3ab29583d9d38babf2994df7220c21",
+          name: "TeddyDoge",
+          symbol: "TEDDY",
+          imageUri: TokenIcon,
+          resourceId:
+            "0x000000000000000000000010f6f2b97F3aB29583D9D38BaBF2994dF7220C2100",
+        },
+        // {
+        //   address: "0x1BABf9a2b471127bdfF520C6271e570DBB215C11",
+        //   name: "USDT",
+        //   symbol: "USDT",
+        //   imageUri: USDTIcon,
+        //   resourceId:
+        //     "0x0000000000000000000000b59e2a3FD2b8d057B86100791EA6BFB5fA2240c100",
+        // },
+        // {
+        //   address: "0xb12BC27B77ae3b2C1506fD5D61E9BaAe5aa78B95",
+        //   name: "USDC",
+        //   symbol: "tUSDC",
+        //   imageUri: USDCIcon,
+        //   resourceId:
+        //     "0x00000000000000000000000B9742d1EE856cC64AcDC848191F458089240ABB00",
+        // },
+        // {
+        //   address: "0x388DC12E1d4519685Cf16f786182790BA3965C16",
+        //   name: "SHIBA",
+        //   symbol: "tSHIBA",
+        //   imageUri: SHIBAIcon,
+        //   resourceId:
+        //     "0x00000000000000000000005ca640153CD0Bc9b89bf38132286f4aCC246ebd800",
+        // },
+        // {
+        //   address: "0x03383aBa89F1F091fA8A0106900ABfD00e57b9aD",
+        //   name: "WETH",
+        //   symbol: "tETH",
+        //   imageUri: ETHIcon,
+        //   resourceId:
+        //     "0x000000000000000000000067eF82C01D60FDb4D1af833Cd3bE0625cCcCe6b100",
+        // },
+      ],
+    },
+    {
       chainId: 0,
       networkId: 1,
       name: "Ethereum - ETH",
